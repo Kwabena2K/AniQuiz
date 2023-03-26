ee44b37aa746803324f57b41799817ba5dccd020
Kwabena <kwabenao2k@gmail.com>
2023-03-25 22:00:00 -0400

    Cleared boilerplate and added some logic for restarting the game and if an answer is inputted. Also set useState cases for questions/scores/results etc. Imported 'he' library as well to curb question text errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import he from "he";
+import "./App.css";
 
 function App() {
+  // Properties
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState([]);
+
+  const shuffleAnswers = (answers) => {
+    return answers.sort(() => Math.random() - 0.5);
+  };
+
+  /* A possible answer was clicked */
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  /* Resets the game back to default */
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Page title  */}
+      <h1 className="title">Welcome to AniQuiz</h1>
+
+      {/*  Real time score */}
+      <h1 className="score">
+        Score: <span>{score}</span>
+      </h1>
     </div>
   );
 }
